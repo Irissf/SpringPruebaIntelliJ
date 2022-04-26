3a287e754c5bf1a3a588f09b1363fd040b3c2d72
Irissf <iris.seijo@gmail.com>
2022-04-26 14:55:49 +0200

    descubro List.of()

--- src/main/java/com/pruebaSpring/prueba/Controllers/UsuarioController.java
+++ src/main/java/com/pruebaSpring/prueba/Controllers/UsuarioController.java
@@ -3,12 +3,15 @@ package com.pruebaSpring.prueba.Controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 //indicamos que es un controllador
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "prueba";
+    @RequestMapping(value = "prueba") // localhost:8080/prueba
+    public List<String> prueba(){
+        return List.of("爸爸","妈妈","弟弟");
     }
+
 }
